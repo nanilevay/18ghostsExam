cb8d60837c6f507d9b998aedcbffdb48a58e72c1
nanilev <dianuski99@gmail.com>
2020-02-01 21:46:08 +0000

    Add project files.

---
+++ 18ghostsExam/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _18ghostsExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
